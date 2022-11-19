d0b8fa4536b41a4d6e54beb12c9edb1dd7c9828c
heyimanshu <407himanshu@gmail.com>
2022-11-19 16:52:07 +0530

    Added Translation Box

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput)
+var outputDiv = document.querySelector("#output");
+console.log(outputDiv)
 
 function clickHandler() {
     console.log("clicked!");
