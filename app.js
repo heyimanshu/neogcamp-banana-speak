6e3b1151dc1b5a58f65a0578a9cc3cc9a9a1b2d8
heyimanshu <407himanshu@gmail.com>
2022-11-20 17:16:22 +0530

    Upadated Banana app with APi

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-console.log(outputDiv)
+
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+fucntion getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+fucntion errorHandler(erro) {
+    console.log("error occured", error);
+    alert("something wrong with server! Try again after some time")
+}
 
 function clickHandler() {
     outputDiv.innerText = " ajajjajaj " + txtInput.value;
